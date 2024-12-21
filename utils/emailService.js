ab07612a43c6739cfc5a52628b5e281e8f31a7dc
Rami <lalouirami34@gmail.com>
2024-12-21 17:13:17 +0100

    last

--- utils/emailService.js
+++ utils/emailService.js
@@ -1,10 +1,10 @@
 const { Resend } = require("resend");
 
-const resend = new Resend("re_E5gMjRAA_5CiYCRb7nxVBwuY8XRALc285");
+const resend = new Resend(process.env.RESEND_API);
 
 const sendEmail = (email, subject, letter) => {
   resend.emails.send({
-    from: "noreply@jethings.com",
+    from: process.env.MAIL_FROM,
     to: email,
     subject,
     html: letter,
